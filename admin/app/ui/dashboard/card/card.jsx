c6ad44ef798c2b8ea558267af391b340a9f0612b
OluwadamilolaOd <oluwadamilolaod@ha-shem.com>
2024-04-26 16:14:15 +0100

    UiPage

--- admin/app/ui/dashboard/card/card.jsx
+++ admin/app/ui/dashboard/card/card.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./card.module.css";
 
-const Card = () => {
+const Card = ({ item }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>10.763</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>35%</span>
+          more than previous week
+        </span>
+        {/* <span className={styles.title}>{item.title}</span>
+        <span className={styles.number}>{item.number}</span>
+        <span className={styles.detail}>
+          <span className={item.change > 0 ? styles.positive : styles.negative}>
+            {item.change}%
+          </span>{" "}
+          {item.change > 0 ? "more" : "less"} than previous week
+        </span> */}
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;

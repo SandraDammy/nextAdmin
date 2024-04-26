948d971f05a89061401c717b8405360313cefbbf
OluwadamilolaOd <oluwadamilolaod@ha-shem.com>
2024-04-26 13:47:42 +0100

    sidebar

--- admin/app/dashboard/layout.jsx
+++ admin/app/dashboard/layout.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import Sidebar from "../ui/dashboard/sidebar/sidebar";
-import Navbar from "../ui/dashboard/navbar/navbar";
+import Navbar from "../ui/dashboard/navbar/navbar"
+import Sidebar from "../ui/dashboard/sidebar/sidebar"
+import styles from "../ui/dashboard/dashboard.module.css"
+import Footer from "../ui/dashboard/footer/footer"
 
-const layout = ({ children }) => {
+const Layout = ({children}) => {
   return (
-    <div>
-      <div>
-        <Sidebar />
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar/>
       </div>
-      <div>
-        <Navbar />
+      <div className={styles.content}>
+        <Navbar/>
         {children}
+        <Footer/>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default layout;
+export default Layout;
